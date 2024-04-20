5cee4d14a1f647a0427a2966b19a66ed241086c7
Renato Delgado <a20190271@pucp.edu.pe>
2024-04-19 20:15:03 -0500

    mi primer commit

---
+++ src/main/java/com/example/clase04gtics/Clase04gticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.clase04gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase04gticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Clase04gticsApplication.class, args);
+    }
+
+}
